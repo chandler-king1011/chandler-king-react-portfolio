860211c4346596632d1a9364ca1edbc21303c4db
Chandler King <ckmotox356@gmail.com>
2020-01-08 20:10:24 -0700

    Made some more changes to the media queries and the about page.

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -13,8 +13,29 @@ export default function() {
               <p>
               I am a currently a student at the Bottega DevCamp coding bootcamp. I am striving and working everyday to become a full time software developer. One of my biggest passions in the field is web development. 
               </p>
-              <p>I feel I am best suited as a front end web developer, however I am very comfortable with a lot of backend implementations as well. I am very fond of the React.js library and work with it almost daily. I have a lot of experience working with API's in React and Javascript. Nothing makes me happier than a great looking website that performs even better.</p>
-              <p>If you are looking for someone to build your next website for your new business, or even a blog website you are in the right spot. Hop on over to my contact page and lets talk about your idea's! I am always looking to learn new skills and push my creativity so any challenge is welcome. </p>
+              <p>
+                I feel I am best suited as a front end web developer, however I am very comfortable with a lot of backend implementations as well. I am very fond of the React.js library and work with it almost daily. I have a lot of experience working with API's in React and Javascript. Nothing makes me happier than a great looking website that performs even better.
+                I do have experience with some other coding languages, frameworks, and technologies including:
+                <ul className="technology-list">
+                  <li>Vanilla JavaScript</li>
+                  <li>NodeJS</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>Python</li>
+                  <li>MYSQL</li>
+                  <li>UML</li>
+                  <li>UI Design</li>
+                  <li>Git</li>
+                </ul>
+              </p>
+              <p>
+                I have many small projects I have built using these various technologies which range from a standard full responsive navbar, to a webscraper built using python. Some of my bigger projects include a fully functioning search engine for the Daily Smarty API which was built using ReactJS and Redux. 
+                I built an eCommerce frontend using the same React and Redux as well. I recently finished building an API in NodeJS with login and full CRUD capabilities. My current project is a capstone project for the Bottega DevCamp course. The project is going to be a fully functioning bug tracking system.
+                This system will connect with the previously mentioned API. The UI will feature a fully user dashboard, bug sharing within an organization, and full CRUD capability with bugs and comments.
+              </p>
+              <p>
+                I have am very intrigued by the endless amounts of knowledge in the tech field that is available to learn. I am very open to any opportunity to learn and grow as a developer, and I feel I can be a very valuable asset to a development team.   
+              </p>
               <div className="bottom-links">
                 <a href="/contact-me">Contact Me</a>
               </div>
